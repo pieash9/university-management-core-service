bef2cde8f60db05ef5ac912a85ab5659bea5b416
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-01-12 18:58:18 +0600

    get all faculty

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
+import { FacultyRoutes } from '../modules/faculty/faculty.routes';
 
 const router = express.Router();
 
@@ -18,7 +19,14 @@ const moduleRoutes = [
     path: '/academic-departments',
     route: AcademicFacultyRoutes,
   },
+  {
+    path: '/faculties',
+    route: FacultyRoutes,
+  },
+  // {
+  //   path: '/students',
+  //   route: studentRoutes
+  // },
 ];
-
 moduleRoutes.forEach(route => router.use(route.path, route.route));
 export default router;

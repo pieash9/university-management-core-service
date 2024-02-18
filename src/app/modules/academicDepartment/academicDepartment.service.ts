e835e7a3bb001f5912a80d5d6f59b9f932c27b5e
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-02-18 10:44:09 +0600

    connected academic department with redis

--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -3,7 +3,11 @@ import { paginationHelpers } from '../../../helpers/paginationHelper';
 import { IGenericResponse } from '../../../interfaces/common';
 import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
+import { RedisClient } from '../../../shared/redis';
 import {
+  EVENT_ACADEMIC_DEPARTMENT_CREATED,
+  EVENT_ACADEMIC_DEPARTMENT_DELETED,
+  EVENT_ACADEMIC_DEPARTMENT_UPDATED,
   academicDepartmentRelationalFields,
   academicDepartmentRelationalFieldsMapper,
   academicDepartmentSearchableFields,
@@ -15,7 +19,17 @@ const insertIntoDB = async (
 ): Promise<AcademicDepartment> => {
   const result = await prisma.academicDepartment.create({
     data,
+    include: {
+      academicFaculty: true,
+    },
   });
+
+  if (result) {
+    await RedisClient.publish(
+      EVENT_ACADEMIC_DEPARTMENT_CREATED,
+      JSON.stringify(result)
+    );
+  }
 
   return result;
 };
@@ -24,7 +38,7 @@ const getAllFromDB = async (
   filters: IAcademicDepartmentFilterRequest,
   options: IPaginationOptions
 ): Promise<IGenericResponse<AcademicDepartment[]>> => {
-  const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+  const { limit, page, skip } = paginationHelpers.calculatePagination(options);
   const { searchTerm, ...filterData } = filters;
 
   const andConditions = [];
@@ -64,7 +78,9 @@ const getAllFromDB = async (
     andConditions.length > 0 ? { AND: andConditions } : {};
 
   const result = await prisma.academicDepartment.findMany({
-    include: { academicFaculty: true },
+    include: {
+      academicFaculty: true,
+    },
     where: whereConditions,
     skip,
     take: limit,
@@ -75,7 +91,6 @@ const getAllFromDB = async (
             createdAt: 'desc',
           },
   });
-
   const total = await prisma.academicDepartment.count({
     where: whereConditions,
   });
@@ -117,6 +132,12 @@ const updateOneInDB = async (
       academicFaculty: true,
     },
   });
+  if (result) {
+    await RedisClient.publish(
+      EVENT_ACADEMIC_DEPARTMENT_UPDATED,
+      JSON.stringify(result)
+    );
+  }
   return result;
 };
 
@@ -129,6 +150,12 @@ const deleteByIdFromDB = async (id: string): Promise<AcademicDepartment> => {
       academicFaculty: true,
     },
   });
+  if (result) {
+    await RedisClient.publish(
+      EVENT_ACADEMIC_DEPARTMENT_DELETED,
+      JSON.stringify(result)
+    );
+  }
   return result;
 };
 
